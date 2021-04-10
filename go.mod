bdcf6fc4bd1fcd471eaf6f83ba973fc5974c06ca
zhenik <nikita.zhevnitskiy@gmail.com>
2021-04-10 17:05:56 +0200

    posts in memory

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module com/zhenik/stock-api
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,9 +1,92 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
 )
 
+type Post struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var posts []Post
+
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(posts)
+}
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var post Post
+	_ = json.NewDecoder(r.Body).Decode(&post)
+	post.ID = strconv.Itoa(rand.Intn(1000000))
+	posts = append(posts, post)
+	json.NewEncoder(w).Encode(&post)
+}
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range posts {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Post{})
+}
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+			var post Post
+			_ = json.NewDecoder(r.Body).Decode(&post)
+			post.ID = params["id"]
+			posts = append(posts, post)
+			json.NewEncoder(w).Encode(&post)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
+}
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.Method, r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	fmt.Println("It works!")
+	router := mux.NewRouter()
+	posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+
+	router.Use(loggingMiddleware)
+	http.ListenAndServe(":8000", router)
 }
